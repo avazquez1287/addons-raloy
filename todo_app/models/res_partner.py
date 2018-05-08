e4c67790b181381f59b23552dfec77e9a94f73b7
Arturo Vazquez <avazquez@raloy.com.mx>
2018-05-08 12:30:50 -0500

    Act. Date_line

--- todo_app/models/res_partner.py
+++ todo_app/models/res_partner.py
@@ -6,10 +6,6 @@ class TodoTask(models.Model):
     _name = 'todo.task'
     _description = 'Tareas personales'
 
-    name = fields.Char('Descripcion', required=True)
-    is_done = fields.Boolean('Terminado?')
-    active = fields.Boolean('Activa ?', default=True)
-    user_id = fields.Many2one('res.users', string='Responsable', default=lambda self: self.env.user)
-    team_ids = fields.Many2many('res.partner', string='Equipo')
+    todo_ids = fields.Many2many('todo.task', string='Equipo de Tareas')
 
 
