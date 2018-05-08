929cba19e12d2c901d8a8be8e83f6673e8933467
Arturo Vazquez <avazquez@raloy.com.mx>
2018-05-08 12:48:32 -0500

    Act. Date_line

--- todo_app/models/res_partner.py
+++ todo_app/models/res_partner.py
@@ -2,9 +2,8 @@
 
 from odoo import fields, models
 
-class TodoTask(models.Model):
-    _name = 'todo.task'
-    _description = 'Tareas personales'
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
 
     todo_ids = fields.Many2many('todo.task', string='Equipo de Tareas')
 
