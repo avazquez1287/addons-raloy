2f442e4421903aa28068e9ab0c73045c8ebee241
Arturo Vazquez <avazquez@raloy.com.mx>
2018-05-09 12:25:27 -0500

    Last Stage

--- todo_stage/models/todo_task_model.py
+++ todo_stage/models/todo_task_model.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models, api
+from odoo.exceptions import  ValidationError
 
 
 class TodoTask(models.Model):
@@ -19,3 +20,20 @@ class TodoTask(models.Model):
         )
 
     partner_id = fields.Many2one('res.partner', string='Contacto', related='user_id.partner_id', readonly=True)
+
+    @api.multi
+    def do_clear_done(self):
+        #Esta condición se evalua de der. a izq. y sobre las dos condiciones crea un and con el amperson del inicio
+        domain = ['&',('is_done', '=', True), '|' ('user_id', '=', self.env.uid), ('user_id', '=', False)]
+        #Si la Tarea_terminada == True AND ( Usuario == uid OR Usuario == False)
+        dones = self.search(domain)
+        dones.write({'active': False})
+        return True
+
+    @api.multi
+    def do_toggle_done(self):
+        for task in self:
+            if task.user_id != self.env.user:
+                raise ValidationError('Solo el usuario responsable puede invertir los valores')
+            task.is_done = not task.is_done
+        return True
